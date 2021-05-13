5677c838dd6c74f68cfdb8fab95c51c695a85169
Barcol <r.bigaj@fibcode.com>
2021-05-13 14:37:06 +0200

    updates for rails 6

--- db/migrate/20140724114255_create_documentation_screenshots.rb
+++ db/migrate/20140724114255_create_documentation_screenshots.rb
@@ -1,4 +1,4 @@
-class CreateDocumentationScreenshots < ActiveRecord::Migration[5.2]
+class CreateDocumentationScreenshots < ActiveRecord::Migration[6.0]
   def change
     create_table :documentation_screenshots do |t|
       t.string :alt_text
--- lib/documentation/version.rb
+++ lib/documentation/version.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Documentation
-  VERSION = '2.0.3'
+  VERSION = '3.0.0'
 end
